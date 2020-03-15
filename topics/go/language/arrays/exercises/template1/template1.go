98e0560db413a38b265d6c87373c90d79234ca97
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-14 18:12:53 -0700

    modified:   arrays/exercises/template1/template1.go

--- topics/go/language/arrays/exercises/template1/template1.go
+++ topics/go/language/arrays/exercises/template1/template1.go
@@ -8,16 +8,25 @@
 // Display the string value and address of each element.
 package main
 
+import "fmt"
+
 // Add imports.
 
 func main() {
 
 	// Declare an array of 5 strings set to its zero value.
+	var arr1 [5]string
 
 	// Declare an array of 5 strings and pre-populate it with names.
-
+	arr2 := [5]string{"a", "b", "c", "d", "e"}
 	// Assign the populated array to the array of zero values.
-
+	for i, v := range arr2 {
+		arr1[i] = v
+	}
 	// Iterate over the first array declared.
 	// Display the string value and address of each element.
+	for i, v := range arr1 {
+		fmt.Printf("Value[%s]\tAddress[%p] IndexAddr[%p]\n", v, &v, &arr1[i])
+
+	}
 }
