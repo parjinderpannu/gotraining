60ac4038d08116bfac201a69fe5a426016b562f3
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-05-23 12:48:13 -0700

    channel tempate4

--- topics/go/concurrency/channels/exercises/template4/template4.go
+++ topics/go/concurrency/channels/exercises/template4/template4.go
@@ -6,54 +6,78 @@
 // numbers are received. Tell the workers to shut down before terminating.
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+)
+
 // Add imports.
 
 func main() {
 
 	// Create the channel for sharing results.
-
+	values := make(chan int)
 	// Create a channel "shutdown" to tell goroutines when to terminate.
-
+	shutdown := make(chan struct{})
 	// Define the size of the worker pool. Use runtime.NumCPU to size the pool based on number of processors.
-
+	poolSize := runtime.NumCPU()
 	// Create a sync.WaitGroup to monitor the Goroutine pool. Add the count.
-
+	var wg sync.WaitGroup
+	wg.Add(poolSize)
 	// Create a fixed size pool of goroutines to generate random numbers.
-	{
-		{
+	for i := 0; i < poolSize; i++ {
+		go func(id int) {
 
 			// Start an infinite loop.
-			{
+			for {
 
 				// Generate a random number up to 1000.
-
+				n := rand.Intn(1000)
 				// Use a select to either send the number or receive the shutdown signal.
-				{
+				select {
 
-					// In one case send the random number.
-
+				// In one case send the random number.
+				case values <- n:
+					fmt.Printf("Worker %d sent %d\n", id, n)
 					// In another case receive from the shutdown channel.
-
+				case <-shutdown:
+					fmt.Printf("Worker %d shutting down\n", id)
+					wg.Done()
+					return
 				}
 			}
-		}
+		}(i)
 	}
 
 	// Create a slice to hold the random numbers.
-
+	var nums []int
 	// Receive from the values channel with range.
-	{
+	for i := range values {
 
 		// continue the loop if the value was even.
+		if i%2 == 0 {
+			fmt.Println("Discarding ", i)
+			continue
+		}
 
 		// Store the odd number.
+		fmt.Println("Keeping ", i)
+		nums = append(nums, i)
 
 		// break the loop once we have 100 results.
+		if len(nums) == 100 {
+			break
+		}
 	}
 
 	// Send the shutdown signal by closing the shutdown channel.
-
+	fmt.Println("Receiver sending shutdown signal")
+	close(shutdown)
 	// Wait for the Goroutines to finish.
-
+	wg.Wait()
 	// Print the values in our slice.
+	fmt.Printf("Results count: %d\n", len(nums))
+	fmt.Println(nums)
 }
