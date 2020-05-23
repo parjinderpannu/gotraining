7f34ee2f5f3eeea39318733cedd0223f05c16ace
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-05-23 10:46:40 -0700

    channel exercise 1

--- topics/go/concurrency/channels/exercises/template1/template1.go
+++ topics/go/concurrency/channels/exercises/template1/template1.go
@@ -7,36 +7,64 @@
 // the program cleanly.
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 // Add imports.
 
 func main() {
 
 	// Create an unbuffered channel.
+	share := make(chan int)
 
 	// Create the WaitGroup and add a count
 	// of two, one for each goroutine.
+	var wg sync.WaitGroup
+	wg.Add(2)
 
 	// Launch the goroutine and handle Done.
+	go func() {
+		goroutine("Bill", share)
+		wg.Done()
+	}()
 
 	// Launch the goroutine and handle Done.
-
+	go func() {
+		goroutine("Joan", share)
+		wg.Done()
+	}()
 	// Send a value to start the counting.
-
+	share <- 1
 	// Wait for the program to finish.
+	wg.Wait()
 }
 
 // goroutine simulates sharing a value.
-func goroutine( /* parameters */ ) {
+func goroutine(name string, share chan int) {
 	for {
 
 		// Wait for the value to be sent.
 		// If the channel was closed, return.
+		value, ok := <-share
+		if !ok {
+			fmt.Printf("Goroutine %s Down\n", name)
+			return
+		}
 
 		// Display the value.
+		fmt.Printf("Goroutine %s Inc %d\n", name, value)
 
 		// Terminate when the value is 10.
+		if value == 10 {
+			close(share)
+			fmt.Printf("Goroutine %s Down\n", name)
+			return
+		}
 
 		// Increment the value and send it
 		// over the channel.
+		share <- (value + 1)
 	}
 }
