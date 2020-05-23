86cdf7cbb31c4ebb18f0afe8c1c7206777bd3098
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-05-23 00:09:16 -0700

    data race exercise worked with sync.RWMutex

--- topics/go/concurrency/data_race/exercises/template1/template1.go
+++ topics/go/concurrency/data_race/exercises/template1/template1.go
@@ -14,6 +14,9 @@ import (
 // numbers maintains a set of random numbers.
 var numbers []int
 
+// rwMutex is used to define a critical section of code.
+var rwMutex sync.RWMutex
+
 // init is called prior to main.
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -39,18 +42,48 @@ func main() {
 	// Wait for all the goroutines to finish.
 	wg.Wait()
 
-	// Display the set of random numbers.
-	for i, number := range numbers {
-		fmt.Println(i, number)
+	rwMutex.RLock()
+	{
+		// Display the set of random numbers.
+		for i, number := range numbers {
+			fmt.Println(i, number)
+		}
 	}
+	rwMutex.RUnlock()
 }
 
 // random generates random numbers and stores them into a slice.
 func random(amount int) {
-
-	// Generate as many random numbers as specified.
-	for i := 0; i < amount; i++ {
-		n := rand.Intn(100)
-		numbers = append(numbers, n)
+	rwMutex.Lock()
+	{
+		// Generate as many random numbers as specified.
+		for i := 0; i < amount; i++ {
+			n := rand.Intn(100)
+			numbers = append(numbers, n)
+		}
 	}
+	rwMutex.Unlock()
 }
+
+// ==================
+// WARNING: DATA RACE
+// Read at 0x00000125a1d0 by goroutine 8:
+//   main.random()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:54 +0x92
+//   main.main.func1()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:34 +0x37
+
+// Previous write at 0x00000125a1d0 by goroutine 7:
+//   main.random()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:54 +0xf6
+//   main.main.func1()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:34 +0x37
+
+// Goroutine 8 (running) created at:
+//   main.main()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:33 +0xbc
+
+// Goroutine 7 (finished) created at:
+//   main.main()
+//       /Users/ppannu/code/learn/gotraining/topics/go/concurrency/data_race/exercises/template1/template1.go:33 +0xbc
+// ==================
